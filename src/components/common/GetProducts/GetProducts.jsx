161420108f1d5fc8c604553f90534caf83b9156d
diegoandres22 <diego.a.v3005@gmail.com>
2023-07-01 17:31:59 -0300

    mostrando productos

--- src/components/common/GetProducts/GetProducts.jsx
+++ src/components/common/GetProducts/GetProducts.jsx
@@ -8,32 +8,44 @@ const GetProducts = () => {
     return (
         <div className={style.postcont}>
             <div className={style.filandor}>
-                <p>Filtrar por: </p>
-                <p>Ordenar por: </p>
+                <p>{products.length} productos</p>
+               
+                <p>ordenar por :</p>
             </div>
 
-
-
             <ul className={style.list}>
-
-                <div className={style.titlelist}>
-                    <p className={style.liid}>Id:</p>
-                    <p className={style.liname}>Nombre:</p>
-                    <p>Marca:</p>
-                    <p>Precio:</p>
-                    <p>Stock:</p>
-                    <p>Categoría:</p>
-                    <p>Rating:</p>
-                    <p>Estado:</p>
-                </div>
                 
-                {products.map((pro, index = 1) => {
+                <li className={style.li}>
+
+                    <p className={style.liid}> Id: </p>
+                    <p className={style.liname}>Titulo:</p>
+                    <p className={style.limarc}>Marca:</p>
+                    <p className={style.liprec}>Precio:</p>
+                    <p className={style.listo}>Stock:</p>
+                    <p className={style.licat}>Sub-categ:</p>
+                    <p className={style.lirat}>Rating:</p>
+                    <p className={style.lista}>Estado:</p>
+                </li>
+
+                {products.map((pro, index) => {
+
                     return (
-                        <li key={index} className={style.li}>
-                            <p>{index + 1}</p>
-                            <p>{pro._id}</p>
+                        <li className={style.li}>
+
+                            {/* <p>{index + 1}</p> */}
+
+                            <p className={style.liid}>{pro._id}</p>
+
                             <p className={style.liname}> {pro.name}</p>
-                            <p>{pro.brand}</p>
+
+                            <p className={style.limarc}>{pro.brand}</p>
+
+                            <p className={style.liprec}>{pro.price}</p>
+                            <p className={style.listo}>{pro.stock}</p>
+                            <p className={style.licat}>{pro.subcategories[0]}</p>
+                            <p className={style.lirat}>{pro.rating}</p>
+                            <p className={style.lista}>{pro.active}</p>
+
                         </li>
                     )
                 })}
