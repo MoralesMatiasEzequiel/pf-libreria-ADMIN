5ecc2d96e854e299a6c6b8792ae8379101f20ac0
Matías Morales <programacionfrida@gmail.com>
2023-07-03 14:05:02 -0300

    putUserOK

--- src/App.js
+++ src/App.js
@@ -23,6 +23,27 @@ function App() {
     <div className="App">
       <SideBar />
       <Routes>
+        <Route path='/products' element={<Products />} />
+        <Route path='/products/get' element={<GetProducts />} />
+        <Route path='/products/post' element={<Post />} />
+        <Route path='/products/put' element={<PutPro />} />
+        <Route path='/products/disa' element={<DisaPro />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/users/get' element={<GetUsers />} />
+        <Route path='/users/put' element={<PutUsers />} />
+        <Route path='/users/:id' element={<PutUserID />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+return (
+    <div className="App">
+      <SideBar />
+      <Routes>
         <Route path='/products' element={<Products />}>
           <Route path=':get' element={<GetProducts />} />
           <Route path=':post' element={<Post />} />
@@ -41,6 +62,4 @@ function App() {
       </Routes>
     </div>
   );
-}
-
-export default App;
+*/
--- src/components/common/PutUser/PutUserID.jsx
+++ src/components/common/PutUser/PutUserID.jsx
@@ -7,15 +7,15 @@ import { useParams, Link } from "react-router-dom";
 const PutUserID = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
-  const { userID } = useSelector(state => state.user);
+  const { detail } = useSelector(state => state.user);
   // console.log(userID);
-  const userFound = userID.find(user => user._id === id);
+  // const detail = detail.find(user => user._id === id);
 
   useEffect(() => {
-    // Aquí puedes agregar la lógica para obtener los detalles del producto según su id
+ 
   }, [id, dispatch]);
 
-  if (!userFound) {
+  if (!detail) {
     return <div>Usuario no encontrado</div>;
   }
 
@@ -24,17 +24,17 @@ const PutUserID = () => {
       <div className="row">
         <div className="col-md-6 d-flex align-items-center">
           <div className="card" style={{ width: "100%", height: "450px", display: "flex", justifyContent: "center", alignItems: "center", marginTop: "25px", marginBottom: "25px" }}>
-            <img className="card-img-top" src={userFound.picture} alt={userFound.name} style={{ maxWidth: "300px", maxHeight: "400px" }} />
+            <img className="card-img-top" src={detail.picture} alt={detail.name} style={{ maxWidth: "300px", maxHeight: "400px" }} />
           </div>
         </div>
         <div className="col-md-6 d-flex align-items-center justify-content-center">
           <div className="card" style={{ width: "100%", height: "450px", marginTop: "25px", marginBottom: "25px" }}>
             <div className="card-body" style={{ padding: "10px" }}>
-              <h5 className="card-title" style={{ color: "#191919", fontFamily: "Montserrat, sans-serif", fontWeight: "bold", fontSize: "24px", padding: "10px" }}>{userFound.name}</h5>
-              <p style={{ color: "#3F3F3F", fontSize: "18px", padding: "10px" }}>ID: {userFound._id}</p>
-              <p style={{ color: "#3F3F3F", fontSize: "18px", padding: "10px" }}>Nickname: {userFound.nickname}</p>
-              <p style={{ color: "#3F3F3F", fontSize: "18px", padding: "10px" }}>E-mail: {userFound.email}</p>
-              <p style={{ color: "#3F3F3F", fontSize: "18px", padding: "10px" }}>Status: {userFound.active === true ? "Activo" : "Inactivo"}</p>
+              <h5 className="card-title" style={{ color: "#191919", fontFamily: "Montserrat, sans-serif", fontWeight: "bold", fontSize: "24px", padding: "10px" }}>{detail.name}</h5>
+              <p style={{ color: "#3F3F3F", fontSize: "18px", padding: "10px" }}>ID: {detail._id}</p>
+              <p style={{ color: "#3F3F3F", fontSize: "18px", padding: "10px" }}>Nickname: {detail.nickname}</p>
+              <p style={{ color: "#3F3F3F", fontSize: "18px", padding: "10px" }}>E-mail: {detail.email}</p>
+              <p style={{ color: "#3F3F3F", fontSize: "18px", padding: "10px" }}>Status: {detail.active === true ? "Activo" : "Inactivo"}</p>
             </div>
           </div>
         </div>
