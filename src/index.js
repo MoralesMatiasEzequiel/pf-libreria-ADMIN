421b578d4c7c3683e19f8e191b2098d6bc39bd64
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2023-07-06 13:19:32 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import store from "./redux/store"
 import axios from "axios"
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-axios.defaults.baseURL = "http://localhost:3001";
-// axios.defaults.baseURL = "https://server-plumalibreria.onrender.com";
+//axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://server-plumalibreria.onrender.com";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
